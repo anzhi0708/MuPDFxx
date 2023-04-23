0c56af3d2aaf574603d4573111aa804b4bad7490
Anji <anzhi0708@gmail.com>
2023-04-24 02:19:12 +0900

    update ../src/PDFDocument.hpp

--- src/PDFDocument.hpp
+++ src/PDFDocument.hpp
@@ -14,18 +14,16 @@ public:
     ctx = fz_new_context(nullptr, nullptr, FZ_STORE_UNLIMITED);
     if (fz_file_exists(ctx, file_name)) {
       this->file_name = file_name;
-      stream = fz_open_file(ctx, file_name);
-      doc = pdf_open_document_with_stream(ctx, stream);
+      doc = pdf_open_document(ctx, file_name);
       this->n_pages = fz_count_pages(ctx, (fz_document *)doc);
     } else {
       fz_drop_context(this->ctx);
-      std::cerr << file_name << " not found" << std::endl;
+      std::cerr << "File not found:\n" << file_name << " not found" << std::endl;
       throw std::runtime_error("PDF file not found.");
     }
   }
   ~PDFDocument() {
     fz_drop_document(this->ctx, (fz_document *)this->doc);
-    fz_drop_stream(this->ctx, this->stream);
     fz_drop_context(this->ctx);
   }
   size_t countPages() { return this->n_pages; }
@@ -34,7 +32,6 @@ public:
 private:
   const char *file_name;
   size_t n_pages;
-  fz_stream *stream;
   pdf_document *doc;
   fz_context *ctx;
 };
