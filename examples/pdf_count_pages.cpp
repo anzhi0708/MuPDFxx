df5934b6c4fa32a5493313393daaa88e0e78c46d
Anji <anzhi0708@gmail.com>
2023-04-24 03:01:20 +0900

    update PDFDocument.hpp

--- examples/pdf_count_pages.cpp
+++ examples/pdf_count_pages.cpp
@@ -6,7 +6,7 @@ using namespace MuPDF;
 int main(int argc, char **argv) {
   assert(argc > 1);
   PDFDocument doc{argv[1]};
-  cout << "File: " << doc.getFileName() << endl;
+  cout << "File: " << doc.getFileFullPath() << endl;
   cout << "Pages: " << doc.countPages() << endl;
   return 0;
 }
--- src/PDFDocument.hpp
+++ src/PDFDocument.hpp
@@ -10,15 +10,16 @@ namespace MuPDF {
 class PDFDocument {
 
 public:
-  PDFDocument(const char *file_name) {
+  PDFDocument(const char *file_path) {
     ctx = fz_new_context(nullptr, nullptr, FZ_STORE_UNLIMITED);
-    if (fz_file_exists(ctx, file_name)) {
-      this->file_name = file_name;
-      doc = pdf_open_document(ctx, file_name);
+    if (fz_file_exists(ctx, file_path)) {
+      this->file_full_path = file_path;
+      doc = pdf_open_document(ctx, file_path);
       this->n_pages = fz_count_pages(ctx, (fz_document *)doc);
     } else {
       fz_drop_context(this->ctx);
-      std::cerr << "File not found:\n" << file_name << " not found" << std::endl;
+      std::cerr << "File not found." << std::endl;
+	  std::cerr << file_path << " not found" << std::endl;
       throw std::runtime_error("PDF file not found.");
     }
   }
@@ -27,10 +28,10 @@ public:
     fz_drop_context(this->ctx);
   }
   size_t countPages() { return this->n_pages; }
-  const char *getFileName() { return this->file_name; }
+  const char *getFileFullPath() { return this->file_full_path; }
 
 private:
-  const char *file_name;
+  const char *file_full_path;
   size_t n_pages;
   pdf_document *doc;
   fz_context *ctx;
