f708c6679900ed722fda2fc4b20216ec7745f215
anji <anzhi0708@gmail.com>
2023-04-24 14:12:34 +0900

    update src/PDFDocument.hpp

--- src/PDFDocument.hpp
+++ src/PDFDocument.hpp
@@ -1,6 +1,7 @@
 extern "C" {
 #include <mupdf/fitz.h>
 #include <mupdf/pdf/document.h>
+#include <mupdf/pdf/page.h>
 }
 #include <iostream>
 #include <stdexcept>
@@ -15,7 +16,7 @@ public:
     if (fz_file_exists(ctx, file_path)) {
       this->file_full_path = file_path;
       doc = pdf_open_document(ctx, file_path);
-      this->n_pages = fz_count_pages(ctx, (fz_document *)doc);
+      this->n_pages = pdf_count_pages(ctx, doc);
     } else {
       fz_drop_context(this->ctx);
       std::cerr << "File not found." << std::endl;
@@ -24,7 +25,7 @@ public:
     }
   }
   ~PDFDocument() {
-    fz_drop_document(this->ctx, (fz_document *)this->doc);
+    pdf_drop_document(this->ctx, this->doc);
     fz_drop_context(this->ctx);
   }
   size_t countPages() { return this->n_pages; }
