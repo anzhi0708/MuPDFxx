03483e3683c81751c3687c38ba3c9ea59a86e122
Anji <anzhi0708@gmail.com>
2023-04-25 15:59:01 +0900

    update examples/pdf_get_page_rect.cpp

--- examples/pdf_get_page_rect.cpp
+++ examples/pdf_get_page_rect.cpp
@@ -10,19 +10,16 @@ int main(int argc, char **argv) {
   cout << "File: " << doc.getFileFullPath() << endl;
   cout << "Pages: " << doc.countPages() << endl;
 
+  // These 2 functions are the same.
   // Getting the 1st page
   PDFPage first_page = doc.loadPageAtIndex(0);
-  // Getting page at index 3
+  // Getting the 4th page (page at index 3)
   PDFPage third_page = doc.page(3);
 
   puts("=== Page 1 ===");
 
   printf("X0: %f, X1: %f, Y0: %f, Y1: %f\n", first_page.getX0(),
          first_page.getX1(), first_page.getY0(), first_page.getY1());
-  // or
-  fz_rect rect = first_page.getFitzRect();
-  printf("x0: %f, y0: %f, x1: %f, y1: %f\n", rect.x0, rect.y0, rect.x1,
-         rect.y1);
 
   puts("=== Page 4 ===");
 
