f3a5f7fe4661b19cf6171e7d556eaa88fd3fab03
Anji <anzhi0708@gmail.com>
2023-04-25 16:00:20 +0900

    update pdf_get_page_rect.cpp

--- examples/pdf_get_page_rect.cpp
+++ examples/pdf_get_page_rect.cpp
@@ -13,15 +13,15 @@ int main(int argc, char **argv) {
   // These 2 functions are the same.
   // Getting the 1st page
   PDFPage first_page = doc.loadPageAtIndex(0);
-  // Getting the 4th page (page at index 3)
-  PDFPage third_page = doc.page(3);
+  // Getting the 3rd page (page at index 2)
+  PDFPage third_page = doc.page(2);
 
   puts("=== Page 1 ===");
 
   printf("X0: %f, X1: %f, Y0: %f, Y1: %f\n", first_page.getX0(),
          first_page.getX1(), first_page.getY0(), first_page.getY1());
 
-  puts("=== Page 4 ===");
+  puts("=== Page 3 ===");
 
   printf("X0: %f, Y0: %f, X1: %f, Y1: %f\n", third_page.x0, third_page.y0,
          third_page.x1, third_page.y1);
